17a16cfec6360cedc4adeb3ff99df9172964b474
cowboy8625 <cowboy8625@protonmail.com>
2022-11-19 08:40:24 -0600

    now we have a repl or we can feed it a file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
 fn main() {
-    println!("New Start");
+    std::env::args().nth(1).map_or_else(
+        || one_liner::repl(),
+        |filename| {
+            let src = std::fs::read_to_string(&filename).unwrap_or("".into());
+            match parser::parse(&src, false) {
+                Ok(s) => {
+                    for f in s.iter() {
+                        println!("{f}");
+                    }
+                }
+                Err(e) => {
+                    let span = e
+                        .downcast_ref::<parser::ParserError>()
+                        .map(|i| i.span())
+                        .unwrap_or(0..0);
+                    print!("{}", report::report(&src, span, &e.to_string()));
+                }
+            }
+        },
+    );
 }
