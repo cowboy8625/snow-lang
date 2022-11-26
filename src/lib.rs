67caf2b5f4cd41eee1dd53f171a93c866c4c06c4
cowboy8625 <cowboy8625@yahoo.com>
2022-11-26 08:23:07 -0600

    snowc is now a crate or a binary

---
+++ src/lib.rs
@@ -0,0 +1,3 @@
+pub use snowc_error_messages::*;
+pub use snowc_parse::*;
+pub use snowc_repl::*;
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
+use snowc::*;
 fn main() {
     std::env::args().nth(1).map_or_else(
-        || snowc_repl::repl(),
+        || Repl::default().run().expect("failed to run repl"),
         |filename| {
             let src = std::fs::read_to_string(&filename).unwrap_or("".into());
-            match snowc_parse::parse(&src, false) {
+            match parse(&src, false) {
                 Ok(s) => {
                     for f in s.iter() {
                         println!("{f}");
@@ -14,10 +15,7 @@ fn main() {
                         .downcast_ref::<snowc_parse::error::ParserError>()
                         .map(|i| i.span())
                         .unwrap_or(0..0);
-                    print!(
-                        "{}",
-                        snowc_error_messages::report(&src, span, &e.to_string())
-                    );
+                    print!("{}", report(&src, span, &e.to_string()));
                 }
             }
         },
