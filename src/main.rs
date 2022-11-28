f25fb4d75eb66be6f94f64e98dc1c5d4423b13ec
cowboy8625 <cowboy8625@yahoo.com>
2022-11-27 20:10:48 -0600

    removed graph code from main

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,16 @@
+#![allow(dead_code)]
+use std::fmt;
+use std::{fs::OpenOptions, io::Write};
+
 use snowc::*;
 fn main() {
     std::env::args().nth(1).map_or_else(
         || Repl::default().run().expect("failed to run repl"),
         |filename| {
             let src = std::fs::read_to_string(&filename).unwrap_or("".into());
-            // let dot_file = format!("{}.dot", filename.split(".").collect::<Vec<_>>()[0]);
             match parse(&src, false) {
                 Ok(s) => {
-                    // let node_tree = output_node_tree(&s);
-                    // println!("<{dot_file}>\n{node_tree}");
+                    let node_tree = output_node_tree(&s);
                     for f in s.iter() {
                         println!("{f}");
                     }
@@ -24,57 +26,3 @@ fn main() {
         },
     );
 }
-/*
-digraph {
-compound=true;
-rankdir="LR";
-subgraph cluster_add {
-label="Fn add";
-add -> λx -> λy -> "x + y";
-}
-subgraph cluster_main {
-label="Fn main";
-main;
-}
-
-main -> add [rankdir=LR, lhead=cluster_add];
-}
-*/
-// fn subgraph<'a>(name: &'a str, nodes: &[&'a str]) -> String {
-//     format!(
-//         "{}}}",
-//         nodes.iter().enumerate().fold(
-//             format!("subgraph cluster_{0} {{\nlabel=\"{0}\"", name),
-//             |last, (i, new)| {
-//                 if i == 0 {
-//                     return format!("{new}");
-//                 }
-//                 format!("{last} -> {new}")
-//             }
-//         )
-//     )
-// }
-//
-// fn expr_to_node(expr: &Expr) -> String {
-//     match expr {
-//         Expr::Atom(_) => todo!(),
-//         Expr::Unary(_, _) => todo!(),
-//         Expr::Binary(_, _, _) => todo!(),
-//         Expr::IfElse(_, _, _) => todo!(),
-//         Expr::Clouser(_, _) => todo!(),
-//         Expr::Func(_, _) => todo!(),
-//         Expr::App(_, _) => todo!(),
-//         Expr::Type(_, _) => todo!(),
-//         Expr::TypeDec(_, _) => todo!(),
-//     }
-// }
-//
-// fn output_node_tree(exprs: &[snowc_parse::Expr]) -> String {
-//     let list: Vec<String> = exprs.iter().map(|e| expr_to_node(e)).collect();
-//     format!(
-//         "digraph {{
-// {}
-// }}",
-//         list.join("\n")
-//     )
-// }
