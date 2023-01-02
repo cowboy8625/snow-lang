5b5d0997a6c9930a768bc15f302d6e6f6f8af3e2
cowboy8625 <cowboy8625@yahoo.com>
2023-01-02 09:36:46 -0600

    imporve paring on whitespace
    also reformated test

--- swrt/src/parse/token_op.rs
+++ swrt/src/parse/token_op.rs
@@ -76,26 +76,26 @@ impl FromStr for TokenOp {
             "load" => parse_1reg_u16(tail, TokenOp::Load, head),
             "push" => parse_1reg(tail, TokenOp::Push, head),
             "pop" => parse_1reg(tail, TokenOp::Pop, head),
+            "inc" => parse_1reg(tail, TokenOp::Inc, head),
+            "dec" => parse_1reg(tail, TokenOp::Dec, head),
+            "prti" => parse_1reg(tail, TokenOp::Prti, head),
             "aloc" => parse_1reg(tail, TokenOp::Aloc, head),
             "setm" => parse_2reg(tail, TokenOp::Setm, head),
-            "add" => parse_3reg(tail, TokenOp::Add, head),
-            "sub" => parse_3reg(tail, TokenOp::Sub, head),
-            "div" => parse_3reg(tail, TokenOp::Div, head),
-            "mod" => parse_3reg(tail, TokenOp::Mod, head),
-            "mul" => parse_3reg(tail, TokenOp::Mul, head),
-            "jmp" => parse_loc(tail, TokenOp::Jmp, head),
-            "jeq" => parse_loc(tail, TokenOp::Jeq, head),
-            "jne" => parse_loc(tail, TokenOp::Jne, head),
             "eq" => parse_2reg(tail, TokenOp::Eq, head),
             "neq" => parse_2reg(tail, TokenOp::Neq, head),
             "gt" => parse_2reg(tail, TokenOp::Gt, head),
             "geq" => parse_2reg(tail, TokenOp::Geq, head),
             "lt" => parse_2reg(tail, TokenOp::Lt, head),
             "leq" => parse_2reg(tail, TokenOp::Leq, head),
-            "inc" => parse_1reg(tail, TokenOp::Inc, head),
-            "dec" => parse_1reg(tail, TokenOp::Dec, head),
+            "add" => parse_3reg(tail, TokenOp::Add, head),
+            "sub" => parse_3reg(tail, TokenOp::Sub, head),
+            "div" => parse_3reg(tail, TokenOp::Div, head),
+            "mod" => parse_3reg(tail, TokenOp::Mod, head),
+            "mul" => parse_3reg(tail, TokenOp::Mul, head),
             "prts" => parse_loc(tail, TokenOp::Prts, head),
-            "prti" => parse_1reg(tail, TokenOp::Prti, head),
+            "jmp" => parse_loc(tail, TokenOp::Jmp, head),
+            "jeq" => parse_loc(tail, TokenOp::Jeq, head),
+            "jne" => parse_loc(tail, TokenOp::Jne, head),
             "hlt" => Ok(TokenOp::Hlt),
             "nop" => Ok(TokenOp::Nop),
             _ => Err(Error::Unexpected("not a opcode".into(), head.into())),
@@ -160,125 +160,175 @@ fn parse_1reg_u16<F>(input: &str, t: F, msg: &str) -> Result<TokenOp, Error>
 where
     F: FnOnce(u8, u8, u8) -> TokenOp,
 {
-    let Some((reg, imm)) = input.split_once(' ') else {
+    let Some((reg, imm)) = input.trim_start().split_once(' ') else {
         return Err(Error::Unexpected("load reg imm".into(), input.into()));
     };
-    let Reg(r) = reg.parse::<Reg>()?;
-    let i = imm.trim()
+    let Reg(r) = reg.trim().parse::<Reg>()?;
+    let i = imm
+        .trim()
         .parse::<u32>()
         .map_err(|_| Error::Unexpected(format!("{msg} <num>"), imm.into()))?;
     let [_, _, b2, b1] = i.to_be_bytes();
     Ok(t(r, b2, b1))
 }
 
-#[test]
-fn from_str_opcode_load() {
-    assert_eq!(
-        "load %0 123".parse::<TokenOp>(),
-        Ok(TokenOp::Load(0, 0, 123))
-    );
-}
+#[cfg(test)]
+mod test {
+    use super::*;
+    #[test]
+    fn from_str_opcode_load() {
+        let src = "load    %0      123     ";
+        let left = src.parse::<TokenOp>();
+        let right = Ok(TokenOp::Load(0, 0, 123));
+        assert_eq!(left, right);
+    }
 
-#[test]
-fn from_str_opcode_push() {
-    assert_eq!("push %0".parse::<TokenOp>(), Ok(TokenOp::Push(0)));
-}
+    #[test]
+    fn from_str_opcode_push() {
+        let src = "push      %0     ";
+        let left = src.parse::<TokenOp>();
+        let right = Ok(TokenOp::Push(0));
+        assert_eq!(left, right);
+    }
 
-#[test]
-fn from_str_opcode_pop() {
-    assert_eq!("pop %0".parse::<TokenOp>(), Ok(TokenOp::Pop(0)));
-}
+    #[test]
+    fn from_str_opcode_pop() {
+        let src = "pop     %0    ";
+        let left = src.parse::<TokenOp>();
+        let right = Ok(TokenOp::Pop(0));
+        assert_eq!(left, right);
+    }
 
-#[test]
-fn from_str_opcode_aloc() {
-    assert_eq!("aloc %0".parse::<TokenOp>(), Ok(TokenOp::Aloc(0)));
-}
+    #[test]
+    fn from_str_opcode_aloc() {
+        let src = "aloc     %0   ";
+        let left = src.parse::<TokenOp>();
+        let right = Ok(TokenOp::Aloc(0));
+        assert_eq!(left, right);
+    }
 
-#[test]
-fn from_str_opcode_setm() {
-    assert_eq!("setm %0 %21".parse::<TokenOp>(), Ok(TokenOp::Setm(0, 21)));
-}
+    #[test]
+    fn from_str_opcode_setm() {
+        let src = "setm        %0           %21    ";
+        let left = src.parse::<TokenOp>();
+        let right = Ok(TokenOp::Setm(0, 21));
+        assert_eq!(left, right);
+    }
 
-#[test]
-fn from_str_opcode_add() {
-    assert_eq!("add %0 %1 %2".parse::<TokenOp>(), Ok(TokenOp::Add(0, 1, 2)));
-}
+    #[test]
+    fn from_str_opcode_add() {
+        let src = "add %0 %1 %2";
+        let left = src.parse::<TokenOp>();
+        let right = Ok(TokenOp::Add(0, 1, 2));
+        assert_eq!(left, right);
+    }
 
-#[test]
-fn from_str_opcode_sub() {
-    assert_eq!("sub %0 %1 %2".parse::<TokenOp>(), Ok(TokenOp::Sub(0, 1, 2)));
-}
+    #[test]
+    fn from_str_opcode_sub() {
+        let src = "sub %0 %1 %2";
+        let left = src.parse::<TokenOp>();
+        let right = Ok(TokenOp::Sub(0, 1, 2));
+        assert_eq!(left, right);
+    }
 
-#[test]
-fn from_str_opcode_div() {
-    assert_eq!("div %0 %1 %2".parse::<TokenOp>(), Ok(TokenOp::Div(0, 1, 2)));
-}
+    #[test]
+    fn from_str_opcode_div() {
+        let src = "div %0 %1 %2";
+        let left = src.parse::<TokenOp>();
+        let right = Ok(TokenOp::Div(0, 1, 2));
+        assert_eq!(left, right);
+    }
 
-#[test]
-fn from_str_opcode_mul() {
-    assert_eq!("mul %0 %1 %2".parse::<TokenOp>(), Ok(TokenOp::Mul(0, 1, 2)));
-}
+    #[test]
+    fn from_str_opcode_mul() {
+        let src = "mul %0 %1 %2";
+        let left = src.parse::<TokenOp>();
+        let right = Ok(TokenOp::Mul(0, 1, 2));
+        assert_eq!(left, right);
+    }
 
-#[test]
-fn from_str_opcode_jmp() {
-    assert_eq!(
-        "jmp __start__".parse::<TokenOp>(),
-        Ok(TokenOp::Jmp(Location("__start__".into())))
-    );
-    assert_eq!(
-        "jmp can_you_parse_this".parse::<TokenOp>(),
-        Ok(TokenOp::Jmp(Location("can_you_parse_this".into())))
-    );
-    assert!("jmp 123".parse::<TokenOp>().is_err());
-}
+    #[test]
+    fn from_str_opcode_jmp() {
+        let src = "jmp __start__";
+        let left = src.parse::<TokenOp>();
+        let right = Ok(TokenOp::Jmp(Location("__start__".into())));
+        assert_eq!(left, right);
+        let src = "jmp can_you_parse_this";
+        let left = src.parse::<TokenOp>();
+        let right = Ok(TokenOp::Jmp(Location("can_you_parse_this".into())));
+        assert_eq!(left, right);
+        assert!("jmp 123".parse::<TokenOp>().is_err());
+    }
 
-#[test]
-fn from_str_opcode_jeq() {
-    assert_eq!(
-        "jeq start".parse::<TokenOp>(),
-        Ok(TokenOp::Jeq(Location("start".into())))
-    );
-}
+    #[test]
+    fn from_str_opcode_jeq() {
+        let src = "jeq start";
+        let left = src.parse::<TokenOp>();
+        let right = Ok(TokenOp::Jeq(Location("start".into())));
+        assert_eq!(left, right);
+    }
 
-#[test]
-fn from_str_opcode_jne() {
-    assert_eq!(
-        "jne start".parse::<TokenOp>(),
-        Ok(TokenOp::Jne(Location("start".into())))
-    );
-}
+    #[test]
+    fn from_str_opcode_jne() {
+        let src = "jne start";
+        let left = src.parse::<TokenOp>();
+        let right = Ok(TokenOp::Jne(Location("start".into())));
+        assert_eq!(left, right);
+    }
 
-#[test]
-fn from_str_opcode_eq() {
-    assert_eq!("eq %10 %11".parse::<TokenOp>(), Ok(TokenOp::Eq(10, 11)));
-}
+    #[test]
+    fn from_str_opcode_eq() {
+        let src = "eq %10 %11";
+        let left = src.parse::<TokenOp>();
+        let right = Ok(TokenOp::Eq(10, 11));
+        assert_eq!(left, right);
+    }
 
-#[test]
-fn from_str_opcode_neq() {
-    assert_eq!("neq %10 %11".parse::<TokenOp>(), Ok(TokenOp::Neq(10, 11)));
-}
+    #[test]
+    fn from_str_opcode_neq() {
+        let src = "neq %10 %11";
+        let left = src.parse::<TokenOp>();
+        let right = Ok(TokenOp::Neq(10, 11));
+        assert_eq!(left, right);
+    }
 
-#[test]
-fn from_str_opcode_gt() {
-    assert_eq!("gt %10 %0".parse::<TokenOp>(), Ok(TokenOp::Gt(10, 0)));
-}
+    #[test]
+    fn from_str_opcode_gt() {
+        let src = "gt %10 %0";
+        let left = src.parse::<TokenOp>();
+        let right = Ok(TokenOp::Gt(10, 0));
+        assert_eq!(left, right);
+    }
 
-#[test]
-fn from_str_opcode_geq() {
-    assert_eq!("geq %10 %23".parse::<TokenOp>(), Ok(TokenOp::Geq(10, 23)));
-}
+    #[test]
+    fn from_str_opcode_geq() {
+        let src = "geq %10 %23";
+        let left = src.parse::<TokenOp>();
+        let right = Ok(TokenOp::Geq(10, 23));
+        assert_eq!(left, right);
+    }
 
-#[test]
-fn from_str_opcode_inc() {
-    assert_eq!("inc %10".parse::<TokenOp>(), Ok(TokenOp::Inc(10)));
-}
+    #[test]
+    fn from_str_opcode_inc() {
+        let src = "inc %10";
+        let left = src.parse::<TokenOp>();
+        let right = Ok(TokenOp::Inc(10));
+        assert_eq!(left, right);
+    }
 
-#[test]
-fn from_str_opcode_dec() {
-    assert_eq!("dec %20".parse::<TokenOp>(), Ok(TokenOp::Dec(20)));
-}
+    #[test]
+    fn from_str_opcode_dec() {
+        let src = "dec %20";
+        let left = src.parse::<TokenOp>();
+        let right = Ok(TokenOp::Dec(20));
+        assert_eq!(left, right);
+    }
 
-#[test]
-fn from_str_opcode_hlt() {
-    assert_eq!("hlt".parse::<TokenOp>(), Ok(TokenOp::Hlt));
+    #[test]
+    fn from_str_opcode_hlt() {
+        let src = "hlt";
+        let left = src.parse::<TokenOp>();
+        let right = Ok(TokenOp::Hlt);
+        assert_eq!(left, right);
+    }
 }
